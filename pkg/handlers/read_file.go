c8a1cf95a82b96a5f34bdab8f506f12b502c52a4
Artem Moskvin <artemm@spotify.com>
2024-04-24 22:30:52 +0200

    Add API for file manipulation

---
+++ pkg/handlers/read_file.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"path/filepath"
+
+	"github.com/artmoskvin/hide/pkg/filemanager"
+	"github.com/artmoskvin/hide/pkg/project"
+)
+
+type ReadFileHandler struct {
+	Manager     project.Manager
+	FileManager filemanager.FileManager
+}
+
+func (h ReadFileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	projectId := r.PathValue("id")
+	filePath := r.PathValue("path")
+
+	project, err := h.Manager.GetProject(projectId)
+
+	if err != nil {
+		http.Error(w, "Project not found", http.StatusNotFound)
+		return
+	}
+
+	fullPath := filepath.Join(project.Path, filePath)
+	file, err := h.FileManager.ReadFile(fullPath)
+
+	if err != nil {
+		http.Error(w, "Failed to read file", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(file)
+}
