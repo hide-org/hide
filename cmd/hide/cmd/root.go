872f01ae7383b79243822113767b5b4beb695aec
aleh <aleh.plashchynski@formulate.app>
2024-08-18 13:25:40 +0200

    run

--- cmd/hide/cmd/root.go
+++ cmd/hide/cmd/root.go
@@ -1,168 +1,35 @@
 package cmd
 
 import (
-	"context"
-	"errors"
 	"fmt"
-	"net/http"
 	"os"
-	"os/signal"
-	"path/filepath"
-	"syscall"
-	"time"
 
-	"github.com/docker/docker/client"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	"github.com/savioxavier/termlink"
 	"github.com/spf13/cobra"
 
 	"github.com/artmoskvin/hide/cmd/hide/pkg/config"
-	"github.com/artmoskvin/hide/pkg/devcontainer"
-	"github.com/artmoskvin/hide/pkg/files"
-	"github.com/artmoskvin/hide/pkg/handlers"
-	"github.com/artmoskvin/hide/pkg/lsp"
-	"github.com/artmoskvin/hide/pkg/model"
-	"github.com/artmoskvin/hide/pkg/project"
-	"github.com/artmoskvin/hide/pkg/util"
-)
-
-const (
-	ProjectsDir       = "hide-projects"
-	DefaultDotEnvPath = ".env"
-)
-
-var (
-	envPath string
-	debug   bool
-	port    int
 )
 
 func init() {
 	cobra.EnableTraverseRunHooks = true
 
-	pf := rootCmd.PersistentFlags()
-	pf.StringVar(&envPath, "env", DefaultDotEnvPath, "path to the .env file")
-	pf.BoolVar(&debug, "debug", false, "run service in a debug mode")
-	pf.IntVar(&port, "port", 8080, "service port")
+	rootCmd.AddCommand(runCmd)
 }
 
 var rootCmd = &cobra.Command{
 	Use: "hide",
 	Long: fmt.Sprintf(`%s
-Hide is a headless IDE for coding agents
-	`, splash),
+%s is a headless IDE for coding agents.
+	`, splash, termlink.ColorLink("Hide", "https://hide.sh", "blue")),
 	CompletionOptions: cobra.CompletionOptions{
 		HiddenDefaultCmd: true,
 	},
 	Version: config.Version(),
-	PreRun: func(cmd *cobra.Command, args []string) {
-		setupLogger(debug)
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print(splash)
-
-		_, err := os.Stat(envPath)
-
-		if os.IsNotExist(err) {
-			log.Debug().Msgf("Environment file %s does not exist.", envPath)
-		}
-
-		if err == nil {
-			dir, file := filepath.Split(envPath)
-
-			if dir == "" {
-				dir = "."
-			}
-
-			err := util.LoadEnv(os.DirFS(dir), file)
-			if err != nil {
-				log.Error().Err(err).Msgf("Cannot load environment variables from %s", envPath)
-			}
-		}
-
-		dockerUser := os.Getenv("DOCKER_USER")
-		dockerToken := os.Getenv("DOCKER_TOKEN")
-
-		if dockerUser == "" || dockerToken == "" {
-			log.Warn().Msg("DOCKER_USER or DOCKER_TOKEN environment variables are empty. This might cause problems when pulling images from Docker Hub.")
-		}
-
-		dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-		if err != nil {
-			log.Fatal().Err(err).Msg("Cannot initialize docker client")
-		}
-
-		ctx := cmd.Context()
-
-		containerRunner := devcontainer.NewDockerRunner(dockerClient, util.NewExecutorImpl(), ctx, devcontainer.DockerRunnerConfig{Username: dockerUser, Password: dockerToken})
-		projectStore := project.NewInMemoryStore(make(map[string]*model.Project))
-		home, err := os.UserHomeDir()
-		if err != nil {
-			log.Fatal().Err(err).Msg("User's home directory is not set")
-		}
-
-		projectsDir := filepath.Join(home, ProjectsDir)
-
-		lspServerExecutables := make(map[lsp.LanguageId]lsp.Command)
-		lspServerExecutables[lsp.LanguageId("go")] = lsp.NewCommand("gopls", []string{})
-		lspServerExecutables[lsp.LanguageId("python")] = lsp.NewCommand("pyright-langserver", []string{"--stdio"})
-
-		fileManager := files.NewFileManager()
-		languageDetector := lsp.NewFileExtensionBasedLanguageDetector()
-		lspService := lsp.NewService(languageDetector, lspServerExecutables)
-		projectManager := project.NewProjectManager(containerRunner, projectStore, projectsDir, fileManager, lspService, languageDetector)
-
-		router := handlers.Router(projectManager)
-
-		addr := fmt.Sprintf("127.0.0.1:%d", port)
-
-		server := &http.Server{
-			Handler:      router,
-			Addr:         addr,
-			WriteTimeout: 15 * time.Second,
-			ReadTimeout:  15 * time.Second,
-		}
-
-		log.Info().Msgf("Server started on %s\n", addr)
-
-		go func() {
-			sigChan := make(chan os.Signal, 1)
-			signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-			<-sigChan
-
-			ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
-			defer cancel()
-
-			log.Info().Msg("Server shutting down ...")
-			if err := projectManager.Cleanup(ctx); err != nil {
-				log.Warn().Err(err).Msgf("Failed to cleanup projects")
-			}
-
-			if err := server.Shutdown(ctx); err != nil {
-				log.Warn().Err(err).Msgf("HTTP shutdown error: %v", err)
-			}
-		}()
-
-		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-			log.Fatal().Err(err).Msgf("HTTP server error: %v", err)
-		}
-	},
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
-	}
-}
-
-func setupLogger(debug bool) {
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339Nano}
-	log.Logger = log.Output(output).With().Caller().Logger()
-
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	if debug {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	}
 }
--- go.mod
+++ go.mod
@@ -21,13 +21,17 @@ require (
 	github.com/distribution/reference v0.6.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/patternmatcher v0.6.0 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
@@ -36,17 +40,30 @@ require (
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/savioxavier/termlink v1.4.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.19.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.51.0 // indirect
 	go.opentelemetry.io/otel v1.26.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.26.0 // indirect
 	go.opentelemetry.io/otel/metric v1.26.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.26.0 // indirect
 	go.opentelemetry.io/otel/trace v1.26.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gotest.tools/v3 v3.5.1 // indirect
 )
