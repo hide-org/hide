cb3cd769ab9e90c87c63fe6fc2e597d65846d164
Aleh Plashchynski <aleh.plashchynski@formulate.app>
2024-11-16 18:06:24 +0100

    pkg/tasks: new architecture implementation for tasks (#162)
    * tasks: add service
    
    * pkg/tasks: finilize implementation
    
    * clean up
    
    ---------
    
    Co-authored-by: Art Moskvin <amoskvin@me.com>

---
+++ pkg/tasks/executor.go
@@ -0,0 +1,77 @@
+package task
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os/exec"
+
+	"github.com/rs/zerolog/log"
+)
+
+type Executor interface {
+	Run(command []string, dir string) (result Result, err error)
+}
+
+type ExecutorImpl struct{}
+
+func NewExecutorImpl() Executor {
+	return &ExecutorImpl{}
+}
+
+func (e *ExecutorImpl) Run(command []string, dir string) (result Result, err error) {
+	log.Debug().Msgf("> %s", command)
+
+	if len(command) == 0 {
+		return result, fmt.Errorf("command is empty")
+	}
+
+	cmnd := command[0]
+
+	var args []string
+	if len(command) > 0 {
+		args = command[1:]
+	}
+
+	cmd := exec.Command(cmnd, args...)
+	cmd.Dir = dir
+
+	err = cmd.Run()
+	if errors.Is(err, &exec.ExitError{}) {
+		v := err.(*exec.ExitError)
+		result.ExitCode = v.ExitCode()
+	}
+	if err != nil {
+		return result, err
+	}
+
+	stdOut, err := cmd.StdoutPipe()
+	if err != nil {
+		return result, err
+	}
+	stdOutStr, err := readAll(stdOut)
+	if err != nil {
+		return result, err
+	}
+	result.StdOut = stdOutStr
+
+	stdErr, err := cmd.StderrPipe()
+	if err != nil {
+		return result, err
+	}
+	stdErrStr, err := readAll(stdErr)
+	if err != nil {
+		return result, err
+	}
+	result.StdErr = stdErrStr
+
+	return
+}
+
+func readAll(src io.Reader) (string, error) {
+	out, err := io.ReadAll(src)
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
---
+++ pkg/tasks/service.go
@@ -0,0 +1,96 @@
+package task
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+type Result struct {
+	StdOut   string `json:"stdout"`
+	StdErr   string `json:"stderr"`
+	ExitCode int    `json:"exitCode"`
+}
+
+type Task struct {
+	Alias   string `json:"alias"`
+	Command string `json:"command"`
+}
+
+type Service interface {
+	Get(ctx context.Context, alias string) (Task, error)
+	List(ctx context.Context) ([]Task, error)
+	Run(ctx context.Context, alias string) (Result, error)
+	RunCommand(ctx context.Context, command string) (Result, error)
+	Upsert(ctx context.Context, alias, command string) (Task, error)
+}
+
+type ServiceImpl struct {
+	executor Executor
+	store    map[string]Task
+}
+
+func NewService(executor Executor, store map[string]Task) Service {
+	return ServiceImpl{
+		executor: executor,
+		store:    store,
+	}
+}
+
+func (s ServiceImpl) Get(ctx context.Context, alias string) (Task, error) {
+	task, ok := s.store[alias]
+	if !ok {
+		return Task{}, NewTaskNotFoundError(alias)
+	}
+	return task, nil
+}
+
+func (s ServiceImpl) List(ctx context.Context) ([]Task, error) {
+	tasks := make([]Task, 0, len(s.store))
+	for _, task := range s.store {
+		tasks = append(tasks, task)
+	}
+	return tasks, nil
+}
+
+func (s ServiceImpl) Upsert(ctx context.Context, alias, command string) (Task, error) {
+	s.store[alias] = Task{Alias: alias, Command: command}
+	return s.Get(ctx, alias)
+}
+
+func (s ServiceImpl) Run(ctx context.Context, alias string) (Result, error) {
+	task, err := s.Get(ctx, alias)
+	if err != nil {
+		return Result{}, err
+	}
+
+	return s.RunCommand(ctx, task.Command)
+}
+
+func (s ServiceImpl) RunCommand(ctx context.Context, command string) (Result, error) {
+	log.Debug().Msgf("Creating task for command: %s", command)
+
+	result, err := s.executor.Run(cmdMaybeWithTimeout(ctx, command), "/workspace")
+	if err != nil {
+		log.Error().Err(err).Msgf("Failed to execute command '%s'", command)
+		return Result{}, fmt.Errorf("failed to execute command: %w", err)
+	}
+	log.Debug().Msgf("Task for command %s completed", command)
+
+	return result, nil
+}
+
+// cmdMaybeWithTimeout prepends timeout command to the command.
+//
+// Note: this is a workaround to ensure that the process is actually stopped after the timeout duration exceeded.
+func cmdMaybeWithTimeout(ctx context.Context, cmd string) (command []string) {
+	deadline, ok := ctx.Deadline()
+	if !ok {
+		return []string{"/bin/bash", "-c", cmd}
+	}
+
+	duration := time.Until(deadline)
+	return []string{"timeout", "--kill-after=1s", "--verbose", fmt.Sprintf("%fs", duration.Seconds()), "/bin/bash", "-c", cmd}
+}
