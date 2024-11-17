5c5bc0bdc5688e36b2de8b4824f4b2c564971183
Art Moskvin <amoskvin@me.com>
2024-11-17 13:43:55 +0100

    pkg/handlers: add project handlers in v2

---
+++ pkg/handlers/v2/delete_project.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/hide-org/hide/pkg/project/v2"
+)
+
+type DeleteProjectHandler struct {
+	Manager project.Manager
+}
+
+func (h DeleteProjectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	projectID, err := getProjectID(r)
+	if err != nil {
+		http.Error(w, "invalid project ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.Manager.DeleteProject(r.Context(), projectID); err != nil {
+		var projectNotFoundError *project.ProjectNotFoundError
+		if errors.As(err, &projectNotFoundError) {
+			http.Error(w, projectNotFoundError.Error(), http.StatusNotFound)
+			return
+		}
+
+		http.Error(w, fmt.Sprintf("Failed to delete project: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
