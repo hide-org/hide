7bd8ab5a9005fbcb8ba5ebe3b0f9a64588d5ae9e
aleh <aleh.plashchynski@formulate.app>
2024-11-18 06:37:11 +0100

    tmp

--- pkg/lsp/delegate.go
+++ pkg/lsp/delegate.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Delegate interface {
-	Get(ctx context.Context, url string) (*http.Response, error)
-	// ProjectRootPath() string
-	// ReadFile(ctx context.Context, path string) ([]byte, error)
+	Get(ctx context.Context, req http.Request) (*http.Response, error)
+	ProjectRootPath() string
+	ReadFile(ctx context.Context, path string) ([]byte, error)
 
 	MakeInstallPath(ctx context.Context, lspName string, version string) (path string, error error)
 }
