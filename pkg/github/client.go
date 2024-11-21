3ba1fe0f2278dce1468dbaf06e4c27b9af3a1741
Art Moskvin <amoskvin@me.com>
2024-11-21 11:08:26 +0100

    pkg/github: move client to separate file

---
+++ pkg/github/client.go
@@ -0,0 +1,38 @@
+package github
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Client interface {
+	GetLatestRelease() (*Release, error)
+}
+
+type ClientImpl struct {
+	owner string
+	repo  string
+}
+
+func (c *ClientImpl) GetLatestRelease() (*Release, error) {
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", c.owner, c.repo)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch latest release: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var release Release
+	if err := json.Unmarshal(body, &release); err != nil {
+		return nil, fmt.Errorf("failed to parse release info: %w", err)
+	}
+
+	return &release, nil
+} 
--- pkg/github/release.go
+++ pkg/github/release.go
@@ -1,10 +1,7 @@
 package github
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"strings"
 )
 
@@ -25,33 +22,3 @@ func (r *Release) GetAssetURL(arch string) (string, error) {
 	}
 	return "", fmt.Errorf("no matching binary found for architecture: %s", arch)
 }
-
-type Client interface {
-	GetLatestRelease() (*Release, error)
-}
-
-type ClientImpl struct {
-	owner string
-	repo  string
-}
-
-func (c *ClientImpl) GetLatestRelease() (*Release, error) {
-	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", c.owner, c.repo)
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch latest release: %w", err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %w", err)
-	}
-
-	var release Release
-	if err := json.Unmarshal(body, &release); err != nil {
-		return nil, fmt.Errorf("failed to parse release info: %w", err)
-	}
-
-	return &release, nil
-} 
